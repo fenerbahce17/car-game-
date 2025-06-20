b5bc3ea6772d47272213e432c85ea1d8b93f0794
zeyagsen1 <ibrahimyesilagac0@gmail.com>
2025-06-20 21:02:09 +0300

    updates

--- Assets/Scripts/Car_Move.cs
+++ Assets/Scripts/Car_Move.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class Car_Move : MonoBehaviour
@@ -5,15 +6,19 @@ public class Car_Move : MonoBehaviour
 
         public float speed = 5f;
 
-        private void Start() {
+        private void Start()
+        {
 
 
         }
 
-        private void Update() {
+        private void Update()
+        {
 
 
                 Move();
+                Turn();
+
         }
 
         public void Move()
@@ -27,9 +32,30 @@ public class Car_Move : MonoBehaviour
                 {
                         transform.Translate(speed * Time.deltaTime * Vector3.back);
                 }
-        
 
-    }
+
+        }
+        Vector3 direction = Vector3.zero;
+        public void Turn()
+        {
+
+                if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+                {
+                        direction = transform.right;
+                }
+                else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+                {
+                        direction = -Vector3.right;
+                }
+                else
+                {
+                        direction = Vector3.zero;
+                }
+            
+                transform.Translate(direction * speed * Time.deltaTime);
+       
+        }
+        
 
 
 }
